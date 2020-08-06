0f17ae2fe10933908e3df83f1fbbc1ff349c862e
Angelica Zubieta <angelicazubieta@gmail.com>
2020-08-06 17:32:23 -0300

    plugins react, import-jsx-a11y-react-parcel-bundler

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cocktail",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  return (
+    <div id="something-important">
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cocktail" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
+  // return React.createElement("div", { id: "something-important" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cocktail",
+  //   }),
+  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
+  // ]);
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
